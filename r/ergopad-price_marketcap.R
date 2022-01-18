b1d78e8b6d0cb7a1bbbcad084313d0ba6c822209
Eeysirhc <cyee428@gmail.com>
2022-01-18 11:12:01 -0800

    viz updates

--- r/ergopad-price_marketcap.R
+++ r/ergopad-price_marketcap.R
@@ -6,8 +6,8 @@
 ## https://ergopad.io/token
 ## https://www.coingecko.com/en/coins/thorstarter
 ## https://www.coingecko.com/en/coins/adapad
-## https://www.coingecko.com/en/coins/bscpad
 ## https://www.coingecko.com/en/coins/velaspad
+## https://www.coingecko.com/en/coins/bscpad
 ## https://www.coingecko.com/en/coins/polkastarter
 ##############################
 
@@ -33,11 +33,11 @@ ergopad_mcap <- 800000/1e6
 
 xrune <- 0.170756
 adapad <- 0.192306
-bscpad <- 1.47
 vlxpad <- 0.451250
+bscpad <- 1.47
 pols <- 2.08
 
-p <- c(xrune, adapad, bscpad, vlxpad, pols)
+p <- c(xrune, adapad, vlxpad, bscpad, pols)
 
 # SIMULATE DATA
 ergopad <- price_sim(min(p), max(p))
@@ -51,8 +51,12 @@ ergopad %>%
   scale_y_continuous(labels = dollar_format(round(2))) +
   scale_x_continuous(labels = dollar_format()) +
   labs(x = "Price", y = "Market Capitalization (millions)",
-       subtitle = "Uniform distribution of price averages for $XRUNE, $ADAPAD, $BSCPAD, $VLXPAD, $POLS",
+       subtitle = "Uniform distribution of price averages for Thorstarter, ADAPad, VelasPad, BSCPAD, and Polkastarter",
        title = "ErgoPad: potential market capitalization based on price target",
        caption = "$0.04 IDO at $800K market cap") +
+  annotate(geom = "text", x = xrune, y = 0.50, color = 'red', label = "$XRUNE\n$ADAPAD") +
+  annotate(geom = "text", x = vlxpad, y = 0.50, color = 'red', label = "$VLXPAD") +
+  annotate(geom = "text", x = bscpad, y = 0.50, color = 'red', label = "$BSCPAD") +
+  annotate(geom = "text", x = pols, y = 0.50, color = 'red', label = "$POLS") +
   expand_limits(ymin = 0, ymax = 50, xmin = 0, xmax = 2.5) +
-  theme_bw(base_size = 15)
+  theme_bw(base_size = 20)
