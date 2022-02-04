a75a6ae4f6a1666c3b7266276a5c33604a83305e
Eeysirhc <cyee428@gmail.com>
2022-02-04 15:25:26 -0800

    repo cleanup

--- sql/ergopad-ergodex_price.sql
+++ sql/ergopad-ergodex_price.sql
@@ -16,7 +16,7 @@ gindex as global_index
 FROM pools p
 LEFT JOIN assets a_x ON a_x.id = p.x_id
 LEFT JOIN assets a_y ON a_y.id = p.y_id
-WHERE pool_id = '61a579c46d92f2718576fc9839a2a1983f172e889ec234af8504b5bbf10edd89' 
+WHERE pool_id = 'd7868533f26db1b1728c1f85c2326a3c0327b57ddab14e41a2b77a5d4c20f4b2'
 ORDER BY gindex ASC
 
 
